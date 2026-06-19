57633a49403d425a9e0b65433dfa185acf535dff
Heorhii Babchenko <zor1kk88@gmail.com>
2026-06-19 19:10:28 +0200

    Fix: fix code style and comments according to flake8 requirements

--- app/main.py
+++ app/main.py
@@ -2,6 +2,7 @@ lucky_number = 777
 pi = 3.14
 one_is_a_prime_number = False
 name = "Richard"
+
 my_favourite_films = [
     "The Shawshank Redemption",
     "The Lord of the Rings: The Return of the King",
@@ -9,27 +10,28 @@ my_favourite_films = [
     "The Good, the Bad and the Ugly",
     "The Matrix",
 ]
+
 profile_info = ("michel", "michel@gmail.com", "12345678")
+
 marks = {
     "John": 4,
     "Sergio": 3,
 }
+
 collection_of_coins = {1, 2, 25}
 
-# create a  dictionary categorized by data types
+# Create a dictionary categorized by data types
 sorted_variables = {
-    "mutable" : [
-        my_favourite_films,  #List[]
+    "mutable": [
+        my_favourite_films,  # List []
         marks,  # Dictionary {}
-        collection_of_coins,  # Set {} , with single values
+        collection_of_coins,  # Set {}, with single values
     ],
-    "immutable" : [
+    "immutable": [
         lucky_number,  # Integer (int)
-        one_is_a_prime_number,  #Boolean value (bool)
+        one_is_a_prime_number,  # Boolean value (bool)
         pi,  # Floating-point number (float)
         name,  # String (str)
         profile_info,  # Tuple ()
     ]
 }
-
-
