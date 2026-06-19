b465a645fb27170a086872fbe84c4229ff57bddd
Heorhii Babchenko <zor1kk88@gmail.com>
2026-06-19 18:26:14 +0200

    create sorted_variables dictionary

--- app/main.py
+++ app/main.py
@@ -16,4 +16,20 @@ marks = {
 }
 collection_of_coins = {1, 2, 25}
 
-# write your code here
+# create a  dictionary categorized by data types
+sorted_variables = {
+    "mutable" : [
+        my_favourite_films, #List[]
+        marks, # Dictionary {}
+        collection_of_coins, # Set {} , with single values
+    ],
+    "immutable" : [
+        lucky_number, # Integer (int)
+        one_is_a_prime_number, #Boolean value (bool)
+        pi, # Floating-point number (float)
+        name, # String (str)
+        profile_info, # Tuple ()
+    ]
+}
+
+
